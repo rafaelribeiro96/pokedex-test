429dfb54ec6c09619b342f3ccae38505dcd20429
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-02-09 18:57:38 -0300

    criada função na api para chamar todos pokemons

--- src/services/apiPokemon.jsx
+++ src/services/apiPokemon.jsx
@@ -1,5 +1,11 @@
+const limitPokemons = 50;
+
 const searchPokemon = async (pokemon) => fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
   .then((response) => response.json())
   .then((data) => data);
 
-export default searchPokemon;
+const getPokemons = async (limit = limitPokemons, offset = 0) => fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
+  .then((response) => response.json())
+  .then((data) => data);
+
+export default { searchPokemon, getPokemons };
