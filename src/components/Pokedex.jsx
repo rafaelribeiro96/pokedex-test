640e581b96d445d17410620267c9613404dac126
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-02-09 19:50:12 -0300

    criada funcionalidade de listar os pokemons na tela principal

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -1,10 +1,28 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 
-function Pokedex() {
+function Pokedex(props) {
+  const { pokemons, loading } = props;
   return (
     <div>
-      <h1>Pokedex</h1>
-      <div>Pokemons</div>
+      <div>
+        <h1>Pokedex</h1>
+        <div>Pokemons</div>
+      </div>
+      {loading ? (<div>Loading ...</div>) : (
+        <div className="pokedex-grid">
+          { pokemons && pokemons.map((pokemon, index) => (
+            <div key={ index }>
+              <div>
+                { pokemon.name }
+              </div>
+            </div>
+
+          ))}
+          {' '}
+
+        </div>
+      )}
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import Pokedex from '../components/Pokedex';
 import Searchbar from '../components/Searchbar';
+import { getPokemons } from '../services/apiPokemon';
 
 function Home() {
+  const [loading, setLoading] = React.useState(false);
+  const [pokemons, setPokemons] = React.useState([]);
+
+  const fetchPokemons = async () => {
+    try {
+      setLoading(true);
+      const response = await getPokemons();
+      setPokemons(response);
+      setLoading(false);
+    } catch (error) {
+      console.log('fetchPokemons error: ', error);
+    }
+  };
+
+  useEffect(() => {
+    console.log('carregou');
+    fetchPokemons();
+  }, []);
+
   return (
     <div className="home-container">
       <Navbar />
       <Searchbar />
-      <Pokedex />
+      <Pokedex pokemons={ pokemons.results } loading={ loading } />
     </div>
   );
 }
