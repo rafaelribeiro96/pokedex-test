1b9e189220a512664c8cb9ddd075dc9d0b357aa1
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-02-09 20:59:45 -0300

    adicionada informações de pokemons no grid

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -5,16 +5,24 @@ function Pokemon(props) {
   const { pokemon } = props;
   console.log('pokemons', pokemon);
   return (
-    <div className="pokemon-card">
-      <div>
+    <div className="pokemon-card-grid">
+
+      <div className="pokemon-title-grid">
         <h4>{pokemon.name}</h4>
         <div>
           #
           {pokemon.id}
         </div>
       </div>
-      <div className="pokemon-image-card">
+
+      <div className="pokemon-image-grid">
         <img src={ pokemon.sprites.front_default } alt={ pokemon.name } />
+      </div>
+
+      <div className="pokemon-types-grid">
+        {pokemon.types.map((type, index) => (
+          <div key={ index } className="pokemon-type-text">{type.type.name}</div>
+        ))}
       </div>
     </div>
   );
