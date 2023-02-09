d77babd09d0c3a89022514841f0525a4e42a41de
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-02-09 16:32:33 -0300

    criado componente navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Home />
     </div>
   );
