d4db5ac24d8bb58042a859ff3ae98a5a5e07a1ec
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-02-09 17:15:23 -0300

    criado onChange onButtonClick utilizando UseState no SearchBar

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Searchbar() {
+  const [pokemonSearch, setPokemonSearch] = useState('charizard');
+
+  const onChangeHandler = (event) => {
+    console.log(event.target.value);
+    setPokemonSearch(event.target.value);
+  };
+
+  const onButtonClick = () => {
+    console.log(pokemonSearch);
+  };
+
   return (
     <div className="searchbar-container">
+
       <div className="searchbar">
-        <input type="text" placeholder="Buscar Pokemon" />
+        <input type="text" placeholder="Buscar Pokemon" onChange={ onChangeHandler } />
       </div>
+
+      <div className="div-searchbar-btn">
+        <button
+          className="searchbar-btn"
+          type="button"
+          onClick={ onButtonClick }
+        >
+          Buscar
+        </button>
+      </div>
+
     </div>
   );
 }
