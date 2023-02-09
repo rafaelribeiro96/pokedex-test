e103c46661386b4fede3cefb59d6cbc6d0925487
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-02-09 15:54:52 -0300

    removida logica anterior, startar novamente após já ter o ambiente configurado

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import './App.css';
-import PokemonProvider from './Context/PokemonContext';
 
 function App() {
   return (
-    <PokemonProvider>
-      <div className="App">
-        <h1>Olá mundo</h1>
-      </div>
-    </PokemonProvider>
+    <div className="App">
+      <h1>Olá mundo</h1>
+    </div>
   );
 }
 
