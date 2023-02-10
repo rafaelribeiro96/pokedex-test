898d9c5a51d8c354280786e5c8d5c746deeb23b4
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-02-10 04:39:52 -0300

    correção lint para primeiro deploy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/jsx-indent-props */
 import React, { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar';
