f5d7729454a3f8907168df697f15ffde2892eef4
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-02-09 22:28:02 -0300

    alterada api para tratar erro do novo metodo search

--- src/services/apiPokemon.jsx
+++ src/services/apiPokemon.jsx
@@ -1,8 +1,13 @@
 const limitPokemons = 30;
+const erroNumber200 = 200;
 
-export const searchPokemon = async (pokemon) => fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-  .then((response) => response.json())
-  .then((data) => data);
+export const searchPokemon = async (pokemon) => {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+  if (response.status === erroNumber200) {
+    return response.json();
+  }
+  return null;
+};
 
 export const getPokemons = async (limit = limitPokemons, offset = 0) => fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
   .then((response) => response.json())
