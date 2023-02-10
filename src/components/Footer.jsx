4576f9e14f9b231e0727c34ccdb0745251934654
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-02-10 12:42:55 -0300

    realizado css do footer, inserido links de redes sociais

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
 import './Footer.css';
+import imageGit from '../assets/images/github.svg';
+import imageLinkedin from '../assets/images/linkedin.svg';
 
 function Footer() {
   return (
     <footer className="footer-container">
-      <h2>
-        Desenvolvido por:
+      <div className="div-vazia-footer" />
+      <div className="text-footer">
+        Desenvolvido por
         {' '}
         <a href="https://www.linkedin.com/in/rafaelfeliperibeiro/">Rafael Ribeiro</a>
-      </h2>
+      </div>
+      <div className="icons-footer">
+        <a href="https://github.com/rafaelribeiro96/">
+          <img
+            className="icon-footer icon-git"
+            src={ imageGit }
+            alt="imagem git svg"
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/rafaelfeliperibeiro/">
+          <img
+            className="icon-footer icon-lkd"
+            src={ imageLinkedin }
+            alt="imagem linkedin svg"
+          />
+        </a>
+      </div>
     </footer>
   );
 }
