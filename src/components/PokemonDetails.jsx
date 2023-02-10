ddf392746fdac383217f969e681f16d185eb470f
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-02-10 02:58:45 -0300

    criado arquivo css pokemon Details

--- src/components/PokemonDetails.jsx
+++ src/components/PokemonDetails.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable react/prop-types */
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { searchPokemon } from '../services/apiPokemon';
+import './PokemonDetails.css';
 
 function PokemonDetails() {
   const [pokemon, setPokemon] = useState(null);
   const { pokemonId } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -16,36 +18,48 @@ function PokemonDetails() {
   }, [pokemonId]);
 
   if (!pokemon) {
-    return <div>Loading...</div>;
+    return <div className="loading">Loading...</div>;
   }
 
   return (
-    <div>
+    <div className="container-pokemon-details">
       <h2>Aqui você pode ver mais informações sobre o Pokémon escolhido.</h2>
-      <h1>{pokemon.name}</h1>
-      <img src={ pokemon.sprites.front_default } alt={ pokemon.name } />
-      <img src={ pokemon.sprites.back_default } alt={ pokemon.name } />
-      <p>
-        Pokemon ID:
-        {' '}
-        {pokemon.id}
-      </p>
-      <p>
-        Tipo:
-        {' '}
-        {pokemon.types.map((type) => type.type.name).join(', ')}
-      </p>
-      <p>
-        Peso:
-        {' '}
-        {pokemon.weight}
-      </p>
-      <p>
-        Altura:
-        {' '}
-        {pokemon.height}
-      </p>
+      <button
+        onClick={ () => navigate('/') }
+        type="button"
+        className="btn-back"
+      >
+        Voltar para a pokedex
 
+      </button>
+      <h1 className="title-pokemon-details">{pokemon.name}</h1>
+      <img
+        className="img-front-details"
+        src={ pokemon.sprites.front_default }
+        alt={ pokemon.name }
+      />
+      <div className="pokemons-other-details">
+        <p>
+          Pokemon #:
+          {' '}
+          {pokemon.id}
+        </p>
+        <p>
+          Tipo:
+          {' '}
+          {pokemon.types.map((type) => type.type.name).join(', ')}
+        </p>
+        <p>
+          Peso:
+          {' '}
+          {pokemon.weight}
+        </p>
+        <p>
+          Altura:
+          {' '}
+          {pokemon.height}
+        </p>
+      </div>
     </div>
   );
 }
