a07028f66ec2f6995b3cbf76e15d33b4ca4f39f2
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-02-13 11:52:01 -0300

    corrigido ao clicar na página abrir o pokemon correto

--- src/services/apiPokemon.jsx
+++ src/services/apiPokemon.jsx
@@ -1,4 +1,13 @@
 const limitPokemons = 30;
+const erroNumber200 = 200;
+
+export const searchPokemon = async (pokemon) => {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+  if (response.status === erroNumber200) {
+    return response.json();
+  }
+  return null;
+};
 
 export const getPokemons = async (limit = limitPokemons, offset = 0) => fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
   .then((response) => response.json())
@@ -7,18 +16,3 @@ export const getPokemons = async (limit = limitPokemons, offset = 0) => fetch(`h
 export const getPokemonData = async (url) => fetch(url)
   .then((response) => response.json())
   .then((data) => data);
-
-export async function searchPokemon(pokemon) {
-  try {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=964');
-    const data = await response.json();
-    const result = data.results.find((p) => p.name.includes(pokemon.toLowerCase()));
-    if (!result) {
-      return undefined;
-    }
-    const pokemonData = await getPokemonData(result.url);
-    return pokemonData;
-  } catch (error) {
-    console.log('searchPokemon error: ', error);
-  }
-}
