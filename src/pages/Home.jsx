b9655a41510e8ef1c48ca00c88eb9f79a159a303
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-02-13 12:04:54 -0300

    erro link corrigido

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import Footer from '../components/Footer';
 import Pokedex from '../components/Pokedex';
